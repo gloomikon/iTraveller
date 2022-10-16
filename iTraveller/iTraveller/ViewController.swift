05766d7801e922bc26bf1bb78e25fbfffd776e64
gloomikon <gloomikon@gmail.com>
2022-10-16 12:31:54 +0200

    Add empty project

---
+++ iTraveller/iTraveller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTraveller
+//
+//  Created by Admin on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
